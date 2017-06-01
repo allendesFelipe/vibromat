08d7189863f76936cf0108616fa67d421282e249
Felipe Allendes Sánchez <felipe.allendes@alumnos.uv.cl>
2017-06-01 17:40:17 -0400

    make it better

---
+++ js/index.js
@@ -0,0 +1,73 @@
+$('#open-popup').magnificPopup({
+    items: [
+      {
+        src: './img/Placas.jpg',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: './img/Placas.jpg',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: './img/Placas.jpg',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: './img/Placas.jpg',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: './img/Placas.jpg',
+        title: 'Peter & Paul fortress in SPB'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+
+$('#open-popup1').magnificPopup({
+    items: [
+      {
+        src: 'https://www.youtube.com/watch?v=iOe6dI2JhgU',
+        type: 'iframe' // this overrides default type
+      },
+      {
+     src: './img/Placas.jpg',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+       src: './img/Placas.jpg',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+      src: './img/Placas.jpg',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: './img/Placas.jpg',
+        title: 'Peter & Paul fortress in SPB'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+
+$('#open-popup2').magnificPopup({
+    items: [
+      {
+        src: 'https://www.youtube.com/watch?v=iOe6dI2JhgU',
+        type: 'iframe' // this overrides default type
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+
+
+
